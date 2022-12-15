c18a823fda428bd11b81ba9a92c7ee4a64b402c2
WooHCode <gusdn526@naver.com>
2022-12-16 03:39:23 +0900

    feature menu

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    원룸샵
-    <h4>{{products[0]}} 원룸 </h4>
-    <p>50 만원</p>
+  <div class="menu">
+    <a v-for="menubar in menus" :key="menubar">{{menubar}}</a>
   </div>
 
   <div>
-    <h4>{{products[1]}} 원룸 </h4>
-    <p>60 만원</p>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4>{{products[0]}}</h4>
+    <p>50만원</p>
+    <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{count[0]}}</span>
   </div>
 
   <div>
-    <h4>{{products[2]}} 원룸 </h4>
-    <p>70 만원</p>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4>{{products[1]}}</h4>
+    <p>60만원</p>
+    <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{count[1]}}</span>
+  </div>
+
+  <div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4>{{products[2]}}</h4>
+    <p>70만원</p>
+    <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{count[2]}}</span>
   </div>
 </template>
 
 <script>
 
 
+
 export default {
   name: 'App',
   data () {
     return{
-      products : ['역삼동 원룸', '천호동 원룸','마포구 원룸']
+      count : [0,0,0],
+      products : ['역삼동 원룸','천호동 원룸','마포구 원룸'],
+      costs : ['50만원','60만원','70만원'],
+      menus : ['Home','Shop','About'],
     }
+  },
+  methods: {
+    increase(i){
+      this.count[i]++;
+    },
   },
   components: {
   }
@@ -33,12 +50,27 @@ export default {
 </script>
 
 <style>
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
+
 </style>
